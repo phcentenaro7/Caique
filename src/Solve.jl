e9428dd2ea1ae5d3dc1c1b2eca219ae0d45d64d5
phcentenaro7 <phc7blu@gmail.com>
2023-04-13 23:10:44 -0300

    Update Solve.jl

--- src/Solve.jl
+++ src/Solve.jl
@@ -1,23 +1,19 @@
-include("Simplex.jl")
-
 function solve(lp::LinearProgram; iB::Vector{Int}=Int[], type::Symbol=:min, form::Symbol=:default,
     anticycling::Bool=true, maxiter::Int=100)
-    if form == :default
-        @match type begin
-            :firstPhase => begin
-                return simplex(lp, lp.iB, :firstPhase, anticycling, maxiter)
-            end
-            :min || :max => begin
-                if isempty(iB)
-                    phaseOne = simplex(lp, lp.iB, :firstPhase, anticycling, maxiter)
-                    phaseOne.conclusion == :unfeasible && return phaseOne
-                    phaseOne.conclusion == :tired && return phaseOne
-                    solution = simplex(lp, phaseOne.iB, type, anticycling, maxiter, validRows=phaseOne.validRows)
-                    solution.iter += phaseOne.iter - 1
-                    return solution
-                end
-                return simplex(lp, iB, type, anticycling, maxiter)
-            end 
+    @match type begin
+        :firstPhase => begin
+            return simplex(lp, lp.iB, :firstPhase, anticycling, maxiter)
         end
+        :min || :max => begin
+            if isempty(iB)
+                phaseOne = simplex(lp, lp.iB, :firstPhase, anticycling, maxiter)
+                phaseOne.conclusion == :unfeasible && return phaseOne
+                phaseOne.conclusion == :tired && return phaseOne
+                solution = simplex(lp, phaseOne.iB, type, anticycling, maxiter, validRows=phaseOne.validRows)
+                solution.iter += phaseOne.iter - 1
+                return solution
+            end
+            return simplex(lp, iB, type, anticycling, maxiter)
+        end 
     end
 end
